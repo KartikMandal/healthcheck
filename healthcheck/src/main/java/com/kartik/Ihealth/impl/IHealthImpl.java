af60103a86540ce9780d3c1b3bdffe36bb077b59
Kartik Chandra Mandal <kartik.cse43@gmail.com>
2019-10-28 18:30:39 +0530

    upload

--- healthcheck/src/main/java/com/kartik/Ihealth/impl/IHealthImpl.java
+++ healthcheck/src/main/java/com/kartik/Ihealth/impl/IHealthImpl.java
@@ -125,19 +125,20 @@ public class IHealthImpl implements IHealth {
 		//if(oracle)
 		try{  
 			//step1 load the driver class  
-			Class.forName("oracle.jdbc.driver.OracleDriver");  
+			Class.forName(datasorceInit.getDiverName());  
 			  
 			//step2 create  the connection object  
-			Connection con=DriverManager.getConnection(  
-			"jdbc:oracle:thin:@localhost:1521:xe","system","oracle");  
+			Connection con = DriverManager.getConnection(
+					datasorceInit.getConnectionUrl(),
+					datasorceInit.getUserId(), datasorceInit.getPassword()); 
 			  
 			//step3 create the statement object  
 			Statement stmt=con.createStatement();  
 			  
 			//step4 execute query  
-			ResultSet rs=stmt.executeQuery("select * from emp");  
+			ResultSet rs=stmt.executeQuery(datasorceInit.getQuery());  
 			while(rs.next())  
-			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
 			  
 			//step5 close the connection object  
 			con.close();  
